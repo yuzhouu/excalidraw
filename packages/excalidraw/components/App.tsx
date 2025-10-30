7af69b9ca2b25ac3bcdb5da7b07ad59986eb8fbd
yuzhou <yuzhou.ini@outlook.com>
2025-10-30 14:57:31 +0800

    let isMobileBreakpoint always return false

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -2365,6 +2365,7 @@ class App extends React.Component<AppProps, AppState> {
   };
 
   private isMobileBreakpoint = (width: number, height: number) => {
+    return false;
     return (
       width < MQ_MAX_WIDTH_PORTRAIT ||
       (height < MQ_MAX_HEIGHT_LANDSCAPE && width < MQ_MAX_WIDTH_LANDSCAPE)
