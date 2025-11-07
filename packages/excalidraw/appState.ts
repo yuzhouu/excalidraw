69216ac0b459cd3472187ad93bef923af605a734
yuzhou <yuzhou.ini@outlook.com>
2025-11-07 18:03:11 +0800

    chore: canvas 默认使用透明背景

--- packages/excalidraw/appState.ts
+++ packages/excalidraw/appState.ts
@@ -100,7 +100,7 @@ export const getDefaultAppState = (): Omit<
     editingFrame: null,
     elementsToHighlight: null,
     toast: null,
-    viewBackgroundColor: COLOR_PALETTE.white,
+    viewBackgroundColor: COLOR_PALETTE.transparent,
     zenModeEnabled: false,
     zoom: {
       value: 1 as NormalizedZoomValue,
